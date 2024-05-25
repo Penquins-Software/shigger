75da919ed8e9b317b4ed50eacdafa837cb629110
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-05-25 14:45:32 +1000

    micro commit

--- source/items/list/item_random_rotation.gd
+++ source/items/list/item_random_rotation.gd
@@ -3,6 +3,6 @@ extends Item
 
 
 func use() -> void:
-	_player.camera.rotate_for_time_by_parts(randf_range(360, 720), randf_range(4, 8), randi_range(32, 64))
+	_player.camera.rotate_by_parts_in_bit(randf_range(360, 720), randi_range(16, 24))
 	Message.create(_world, _player.position, "[color=aqua]%s" % tr("ItemRandomRotation"))
 	queue_free()
--- source/player/player_camera.gd
+++ source/player/player_camera.gd
@@ -19,15 +19,14 @@ func rotate_for_time(degrees: float = 180, time: float = 2.0) -> void:
 	rotation_degrees = _default_degrees
 
 
-func rotate_for_time_by_parts(degrees: float = 180, time: float = 2.0, parts: int = 4) -> void:
+func rotate_by_parts_in_bit(degrees: float = 180, parts: int = 4) -> void:
 	_is_rotate = true
 	
 	var part_degrees = degrees / parts
-	var part_time = time / parts
 	
 	for part in parts:
 		rotation_degrees = part_degrees * (part + 1)
-		await get_tree().create_timer(part_time).timeout
+		await RhythmMachine.bit_1_1
 	
 	_is_rotate = false
 	rotation_degrees = _default_degrees
